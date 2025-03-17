b16521a7bddcc906bf26ae082b5b36dc1afae5b4
baksihwan <baksh999@naver.com>
2025-03-17 17:58:04 +0900

    첫 게시

---
+++ src/main/java/com/example/bookrecord_service/BookrecordServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookrecord_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrecordServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookrecordServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookrecord_service/BookrecordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookrecord_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrecordServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
